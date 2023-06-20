618b9d1c6c7d9b4028591c76f7c0cb2500d7bac6
IrinaApolonnik <iaapolonnik@edu.hse.ru>
2023-06-20 12:37:15 +0300

    ==
    ygkc

--- java/open_air.js
+++ java/open_air.js
@@ -1,4 +1,40 @@
 $(document).ready(function(){
+  var $tooltip = $("#tooltip");
+
+  $(document).on("mousemove", function(evt) {
+    $tooltip.css({left: evt.pageX, top: evt.pageY});
+  });
+  $('#locations').hover(function() {
+      $('div#tooltip').fadeIn(500)
+  }, function() {
+      $('div#tooltip').fadeOut(100)
+  });
+
+  $('#locations').scroll(function() {
+      didScroll = true;
+  });
+
+  $('.loc1').fadeOut();
+  $('.loc2').fadeOut();
+  $('.loc3').fadeOut();
+  $('.loc4').fadeOut();
+  $("body").scroll(function () {
+  if ($(this).scrollLeft() > 1300) {
+  $('.loc1').fadeIn();
+  }
+  else {
+  $('.loc1').fadeOut();
+  }
+  });
+  $("body").scroll(function () {
+  if ($(this).scrollLeft() > 4400) {
+  $('.loc2').fadeIn();
+  }
+  else {
+  $('.loc2').fadeOut();
+  }
+  });
+
 
 $(function() {
     var scroll = $('#locations');
@@ -18,6 +54,34 @@ $(function() {
 
 var block_show = false;
 
+// function scrollTracking(){
+// 	if (block_show) {
+// 		return false;
+// 	}
+//
+// 	var wt = $(window).scrollLeft();
+// 	var wh = $(window).width();
+// 	var et = $('.active').offset().top;
+// 	var eh = $('.active').outerHeight();
+// 	var dh = $(document).width();
+//
+// 	if (wt + wh >= et || wh + wt == dh || eh + et < wh){
+// 		block_show = true;
+//
+// 		// Код анимации
+// 		$('.active div:eq(0)').show('fast', function(){
+// 			$(this).next().show('fast', arguments.callee);
+// 		});
+// 	}
+// }
+//
+// $(window).scroll(function(){
+// 	scrollTracking();
+// });
+//
+// $(document).ready(function(){
+// 	scrollTracking();
+// });
 
 
 
